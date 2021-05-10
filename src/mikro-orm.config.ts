3bf10cb9cc056bd2ff366c87c0388232868ef901
Kian van der Meer <kiansinan@gmail.com>
2021-05-10 08:24:27 +0200

    first release

---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,19 @@
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import path from "path";
+import { User } from "./entities/User";
+
+export default {
+    migrations: {
+        path: path.join(__dirname, "./migrations"),
+        pattern: /^[\w-]+\d+\.[tj]s$/,
+    },
+    type: "postgresql",
+    dbName: "discussmyschool",
+    user: "postgres",
+    password: "postgres",
+    debug: !__prod__,
+    entities: [Post, User]
+
+} as Parameters<typeof MikroORM.init>[0];
